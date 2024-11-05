7397d9a464467c41ebf08f35649d09b3ba87d868
Adam YD <adamyd18@gmail.com>
2024-11-05 08:25:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -59,6 +59,7 @@ cd $KERNEL_DIR
 KERNEL_HEAD_HASH=$(git log --pretty=format:'%H' -1)
 
 Patching the KernelSU
+rm -rf KernelSU
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 echo "
 CONFIG_KPROBES=y
