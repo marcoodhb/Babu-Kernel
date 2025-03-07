fe86e2361eb7e4a721c1514c6b29aac2b1cb11ea
Adam YD <adamyd18@gmail.com>
2025-03-07 09:29:46 +0700

    scripts: Cleanup some shit

--- build.sh
+++ build.sh
@@ -3,31 +3,28 @@
 WORKDIR="$(pwd)"
 
 # Clang
-CLANG_DLINK="https://github.com/XeroMz69/Clang/releases/download/Xero-Clang-20.0.0/Xero-Clang-20.0.0git-20241225.tar.gz"
+CLANG_DLINK="https://github.com/XeroMz69/Clang/releases/download/Xero-Clang-20250303.1/Xero-Clang-21.0.0git-20250303.tar.gz"
 CLANG_DIR="$WORKDIR/Clang/bin"
 
 # Kernel Source
 KERNEL_NAME="SigmaKernel"
-KERNEL_GIT="https://github.com/rexix01/kernel_xiaomi_mt6768"
+KERNEL_GIT="https://github.com/XeroMz69/Bumi-Kernel-Tree.git"
 KERNEL_BRANCH="lineage-21"
 KERNEL_DIR="$WORKDIR/$KERNEL_NAME"
 
 # Build
-DEVICE_CODENAME="gale"
-DEVICE_DEFCONFIG="gale_defconfig"
+DEVICE_CODENAME="earth"
+DEVICE_DEFCONFIG="earth_defconfig"
 DEVICE_DEFCONFIG_FILE="$KERNEL_DIR/arch/arm64/configs/$DEVICE_DEFCONFIG"
 
 IMAGE_GZ="$KERNEL_DIR/out/arch/arm64/boot/Image.gz"
 IMAGE_GZ_DTB="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
 DTB="$KERNEL_DIR/out/arch/arm64/boot/dts/mediatek/mt6768.dtb"
-DTBO_EARTH="$KERNEL_DIR/out/arch/arm64/boot/dts/mediatek/gale.dtbo"
+DTBO_EARTH="$KERNEL_DIR/out/arch/arm64/boot/dts/mediatek/earth.dtbo"
 DTBO="$KERNEL_DIR/out/arch/arm64/boot/dtbo.img"
 
-MAKE_BOOTIMG="false"
-PERMISSIVE_BOOTIMG="false"
-
 export KBUILD_BUILD_USER="Xero"
-export KBUILD_BUILD_HOST="XeroMz"
+export KBUILD_BUILD_HOST="XeroMz69"
 
 cd $WORKDIR
 
@@ -48,16 +45,6 @@ echo "Cloning Kernel Source "
 git clone --depth=1 $KERNEL_GIT -b $KERNEL_BRANCH $KERNEL_DIR
 cd $KERNEL_DIR
 KERNEL_HEAD_HASH=$(git log --pretty=format:'%H' -1)
-
-# KernelSU
-echo "Add KernelSU"
-cd $KERNEL_DIR
-rm -rf KernelSU
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
-
-echo "CONFIG_HAVE_KPROBES=y
-CONFIG_KPROBE_EVENTS=y
-CONFIG_KSU=y" >> $DEVICE_DEFCONFIG
 
 # Build Kernel
 echo "Started Compilation"
@@ -94,21 +81,18 @@ make O=out $args -j"$(nproc --all)" | tee "$WORKDIR/out/build.log"
 
 echo "Checking builds"
 if [ ! -e $IMAGE_GZ_DTB ]; then
-    if [ ! -e $IMAGE_GZ ]; then
     echo -e "Build Failed!"
     exit 1
-    fi
 fi
 
 echo "Packing Kernel"
-ZIP_NAME="Sigma-AnyKernel3"
+ZIP_NAME="Sigma-Kernel"
 cd $WORKDIR
 unzip $ZIP_NAME.zip
-cd $WORKDIR/Sigma-AnyKernel3
+cd $WORKDIR/"$ZIP_NAME"
 rm -f dtb
 cp $IMAGE_GZ_DTB .
-cp $IMAGE_GZ .
-cp $DTB $WORKDIR/Sigma-AnyKernel3/dtb
+cp $DTB $WORKDIR/$ZIP_NAME/dtb
 cp $DTBO .
 Build Configs
 # Pack File
@@ -117,7 +101,6 @@ find ./ * -exec touch -m -d "$time" {} \;
 zip -r9 $ZIP_NAME.zip *
 cp *.zip $WORKDIR/out
 
-cp $IMAGE_GZ $WORKDIR/out
 cp $IMAGE_GZ_DTB $WORKDIR/out
 cp $DTBO $WORKDIR/out
 cp $DTBO_EARTH $WORKDIR/out
